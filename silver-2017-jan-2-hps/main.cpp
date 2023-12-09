7e9344c568b5ed7b797f2dc65993ffcf7ecf1337
alantao5056 <alantaolr@outlook.com>
2023-12-09 13:09:55 -0500

    hps code

---
+++ silver-2017-jan-2-hps/main.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    freopen("hps.in", "r", stdin);
+    freopen("hps.out", "w", stdout);
+
+    cin >> N;
+
+    vector<char> bessie(N);
+
+    // counts for right side
+    int h2 = 0;
+    int p2 = 0;
+    int s2 = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> bessie[i];
+        if (bessie[i] == 'H')
+        {
+            h2++;
+        }
+        else if (bessie[i] == 'P')
+        {
+            p2++;
+        }
+        else
+        {
+            s2++;
+        }
+    }
+
+    // counts for left side
+    int h1 = 0;
+    int p1 = 0;
+    int s1 = 0;
+
+    int result = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        // change the current counts
+        if (bessie[i] == 'H')
+        {
+            h1++;
+            h2--;
+        }
+        else if (bessie[i] == 'P')
+        {
+            p1++;
+            p2--;
+        }
+        else
+        {
+            s1++;
+            s2--;
+        }
+
+        // calculate wins
+        int leftWin = max(h1, max(p1, s1));
+        int rightWin = max(h2, max(p2, s2));
+        result = max(result, leftWin + rightWin);
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
